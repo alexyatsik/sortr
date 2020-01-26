ef6bc4cc45ca4431636ad6e123a0aa84ef66801d
Alexeii Yatsenko <alexeii.yatsenko@gmail.com>
2020-01-26 14:59:52 +0200

    added selection algorithm either asc and desc

--- sortr.js
+++ sortr.js
@@ -10,14 +10,14 @@
         insertion: function(order) {
             let arr = this.arr;
 
-            if(order === 'descend') {
-                for(let i = 1; i < arr.length; i++) {
+            if (order === 'descend') {
+                for (let i = 1; i < arr.length; i++) {
                     let current = arr[i];
-                    if(current < arr[i - 1]) {
+                    if (current < arr[i - 1]) {
                         continue;
                     } else {
                         let prev = i - 1;
-                        while(prev >= 0 && arr[prev] < current) {
+                        while (prev >= 0 && arr[prev] < current) {
                             arr[prev + 1] = arr[prev];
                             prev--;
                         }
@@ -25,13 +25,13 @@
                     }
                 }
             } else {
-                for(let i = 1; i < arr.length; i++) {
+                for (let i = 1; i < arr.length; i++) {
                     let current = arr[i];
-                    if(current >= arr[i - 1]) {
+                    if (current >= arr[i - 1]) {
                         continue;
                     } else {
                         let prev = i - 1;
-                        while(prev >= 0 && arr[prev] > current) {
+                        while (prev >= 0 && arr[prev] > current) {
                             arr[prev + 1] = arr[prev];
                             prev--;
                         }
@@ -49,8 +49,18 @@
             let arr = this.arr;
             let sorted;
 
-            if(order === 'descend') {
+            if (order === 'descend') {
+                do {
+                    sorted = true;
 
+                    for (let current = 0; current < arr.length; current++) {
+                        if (arr[current] < arr[current + 1]) {
+                            [arr[current], arr[current + 1]] = [arr[current + 1], arr[current]];
+                            sorted = false;
+                        }
+                    }
+                    
+                } while (!sorted);
             } else {
                 do {
                     sorted = true;
@@ -63,6 +73,39 @@
                     }
                     
                 } while (!sorted);
+            }
+
+            return arr;
+        },
+
+        selection: function(order) {
+
+            let arr = this.arr;
+
+            if (order === 'descend') {
+                for (let currentIndex = 0; currentIndex < arr.length; currentIndex++) {
+                    let biggestIndex = currentIndex;
+                    let counter = currentIndex;
+                    while (counter <= arr.length) {
+                        if (arr[counter] > arr[biggestIndex]) {
+                            biggestIndex = counter;
+                        }
+                        counter++;
+                    }
+                    [arr[currentIndex], arr[biggestIndex]] = [arr[biggestIndex], arr[currentIndex]];
+                }
+            } else {
+                for (let currentIndex = 0; currentIndex < arr.length; currentIndex++) {
+                    let smallestIndex = currentIndex;
+                    let counter = currentIndex;
+                    while (counter <= arr.length) {
+                        if (arr[counter] < arr[smallestIndex]) {
+                            smallestIndex = counter;
+                        }
+                        counter++;
+                    }
+                    [arr[currentIndex], arr[smallestIndex]] = [arr[smallestIndex], arr[currentIndex]];
+                }
             }
 
             return arr;
